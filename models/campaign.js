3a565a50f5fe3623512fe9e586d5dad75b11dd6a
pearngoc <lethanhngoc123a@gmail.com>
2022-12-19 23:21:58 +0700

    complete CRUD for campaign

--- models/campaign.js
+++ models/campaign.js
@@ -1,20 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const campaignSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-    },
-    image: {
-        type: String,
-    },
-    shop:{
-        type: String,
-    },
-    counterpart: {
-        type: mongoose.Schema.Types.ObjectId,
-    }
-})
-module.exports = mongoose.model('Campaign', campaignSchema);
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  numberOfVoucher: {
+    type: Number,
+  },
+  dateBegin: {
+    type: Number,
+  },
+  dateEnd: {
+    type: Number,
+  },
+  games: {
+    type: Array,
+  },
+  status: {
+    type: String,
+  },
+
+  // shop:{
+  //     type: String,
+  // },
+  // counterpart: {
+  //     type: mongoose.Schema.Types.ObjectId,
+  // }
+});
+module.exports = mongoose.model("Campaign", campaignSchema);
--- routes/campaign.js
+++ routes/campaign.js
@@ -1,7 +1,20 @@
-const express = require('express');
-const {createCampaign} = require("../controllers/campaign");
-const {isAuth} = require("../middlewares/auth");
+const express = require("express");
+const {
+  createCampaign,
+  getAllCampaigns,
+  getOneCampaign,
+  updateCampaign,
+  deleteMultipleCampaign,
+  deleteSingleCampaign,
+} = require("../controllers/campaign");
+const { isAuth } = require("../middlewares/auth");
 const router = express.Router();
 
-router.post('/create', isAuth, createCampaign);
+router.post("/campaigns", createCampaign);
+router.get("/campaigns", getAllCampaigns);
+router.get("/campaigns/:id", getOneCampaign);
+router.put("/campaigns", updateCampaign);
+router.delete("/campaigns", deleteMultipleCampaign);
+router.delete("/campaigns/:id", deleteSingleCampaign);
+
 module.exports = router;
