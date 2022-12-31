4f06dd1ed933edaac9b97614fee64a04ac24af2e
pearngoc <lethanhngoc123a@gmail.com>
2022-12-31 23:39:03 +0700

    update employee

--- controllers/employee.js
+++ controllers/employee.js
@@ -25,3 +25,19 @@ exports.getAllEmployees = async (req, res) => {
     res.status(400).send({ success: false, message: e.message });
   }
 };
+
+exports.updateEmployee = async (req, res) => {
+  try {
+    const { _id, ownerID, ...body } = req.body;
+    console.log(req.body);
+    await Employee.find({
+      $and: [{ ownerID: req.user._id }, { _id: req.body._id }],
+    }).updateOne(body);
+    res.status(200).send({
+      success: true,
+      message: "Update a employee successfully",
+    });
+  } catch (e) {
+    res.status(400).send({ success: false, message: e.message });
+  }
+};
--- routes/employee.js
+++ routes/employee.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { createEmployee, getAllEmployees } = require("../controllers/employee");
+const {
+  createEmployee,
+  getAllEmployees,
+  updateEmployee,
+} = require("../controllers/employee");
 const { isAuth, isCounterpart } = require("../middlewares/auth");
 const router = express.Router();
 
 router.post("/", isAuth, isCounterpart, createEmployee);
 router.get("/", isAuth, isCounterpart, getAllEmployees);
-
+router.put("/", isAuth, isCounterpart, updateEmployee);
 module.exports = router;
