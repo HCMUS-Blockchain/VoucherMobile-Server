aef411ff999440dd8d32546f4033f76d73b123fd
Phuc Le <hoangphuc552001@gmail.com>
2023-01-04 10:42:19 +0700

    supplement

--- models/campaign.js
+++ models/campaign.js
@@ -1,45 +1,45 @@
 const mongoose = require("mongoose");
 const campaignSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-  numberOfVoucher: {
-    type: Number,
-  },
-  dateBegin: {
-    type: Number,
-  },
-  dateEnd: {
-    type: Number,
-  },
-  games: {
-    type: Array,
-  },
-  status: {
-    type: String,
-  },
-  userID: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  userJoin: {
-    type: Array,
-  },
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+        },
+        image: {
+            type: String,
+        },
+        numberOfVoucher: {
+            type: Number,
+        },
+        dateBegin: {
+            type: Number,
+        },
+        dateEnd: {
+            type: Number,
+        },
+        games: {
+            type: Array,
+        },
+        status: {
+            type: String,
+        },
+        userID: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        userJoin: {
+            type: Array,
+        },
 // shop:{
-  //     type: String,
-  // },
-  // counterpart: {
-  //     type: mongoose.Schema.Types.ObjectId,
-  // }
-},
-{ timestamps: true }
+        //     type: String,
+        // },
+        // counterpart: {
+        //     type: mongoose.Schema.Types.ObjectId,
+        // }
+    },
+    {timestamps: true}
 );
 module.exports = mongoose.model("Campaign", campaignSchema);
